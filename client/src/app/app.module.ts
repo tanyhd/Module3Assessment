4e0e532ccc0748f018789438ee2b57c69c731cdf
Darren Tan <darren243@hotmail.com>
2022-02-11 09:34:41 +0800

    Task 2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeAddComponent } from './recipe-add/recipe-add.component';
+
+const appRoutes: Routes = [
+  {path: "", component: RecipeListComponent},
+  {path: "recipe/:recipeId", component: RecipeDetailComponent},
+  {path: "add", component: RecipeAddComponent},
+  {path: "**", redirectTo: '/', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeAddComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
